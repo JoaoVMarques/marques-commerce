de92a519b599a88fdacedca8b8337d1e7981131e
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-11-03 18:56:28 -0300

    Testes para ver se as categorias aparecem com sucesso na tela

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import Home from '../pages/Home';
 import renderWithRouter from './helpers/renderWithRouter';
+import CATEGORIES from './mocks/Categories';
 
 describe('Testando /home', () => {
-  it('verifica se o titulo aparece na tela', () => {
-    renderWithRouter(<Home />);
-    const linkElement = screen.getByText(/Marques-commerce/i);
-    expect(linkElement).toBeInTheDocument();
+  describe('Titulo', () => {
+    it('verifica se o titulo aparece na tela', () => {
+      renderWithRouter(<Home />);
+      const linkElement = screen.getByText(/Marques-commerce/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
+
+  describe('Categorias', () => {
+    it('As categorias são chamadas com sucesso', async () => {
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.resolve(CATEGORIES),
+        }),
+      ) as jest.Mock;
+      renderWithRouter(<Home />);
+      const otakuCategory = await screen.findByText(/Otaku/i);
+      expect(otakuCategory).toBeInTheDocument();
+    });
   });
 });
---
+++ src/tests/mocks/Categories.tsx
@@ -0,0 +1,16 @@
+const CATEGORIES = [
+  {
+    id: 'MLB5672',
+    name: 'Otaku',
+  },
+  {
+    id: 'MLB271599',
+    name: 'Agro',
+  },
+  {
+    id: 'MLB1403',
+    name: 'Alimentos e Bebidas',
+  },
+];
+
+export default CATEGORIES;
