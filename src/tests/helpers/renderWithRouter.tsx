07970fabceb05afb641767ba01cad42ed838c0f4
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-11-04 02:40:16 -0300

    Arrumando os testes onde nao existia o context

--- src/tests/helpers/renderWithRouter.tsx
+++ src/tests/helpers/renderWithRouter.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import ProductsProvider from '../../Providers/ProductsProvider';
 
 const renderWithRouter = (component : JSX.Element) => {
   const history = createMemoryHistory();
   return ({
     ...render(
       <MemoryRouter>
-        {component}
+        <ProductsProvider>
+          {component}
+        </ProductsProvider>
       </MemoryRouter>),
     history,
   });
