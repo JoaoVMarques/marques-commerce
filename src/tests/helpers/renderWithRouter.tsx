3b9f367fe239eae9cee8d4e94c339eeaff2f0882
JoaoVMarques <J.v.marques.fernandes@gmail.com>
2022-12-04 04:16:00 -0300

    Consertando testes e mocks

--- src/tests/helpers/renderWithRouter.tsx
+++ src/tests/helpers/renderWithRouter.tsx
@@ -3,16 +3,19 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import ProductsProvider from '../../Providers/ProductsProvider';
+import CategoriesProvider from '../../Providers/CategoriesProvider';
 
 const renderWithRouter = (component : JSX.Element) => {
   const history = createMemoryHistory();
   return ({
     ...render(
-      <MemoryRouter>
-        <ProductsProvider>
-          {component}
-        </ProductsProvider>
-      </MemoryRouter>),
+      <CategoriesProvider>
+        <MemoryRouter>
+          <ProductsProvider>
+            {component}
+          </ProductsProvider>
+        </MemoryRouter>
+      </CategoriesProvider>),
     history,
   });
 };
--- src/tests/mocks/Pictures.ts
+++ src/tests/mocks/Pictures.ts
@@ -2,21 +2,25 @@ import { IPictures } from '../../interfaces/IPictures';
 
 const PICTURES: IPictures[] = [
   {
+    id: 'MLB2122589588',
     variations: [{
       url: 'http://http2.mlstatic.com/D_735964-MLA43406215008_092020-I.jpg'
     }]
   },
   {
+    id: 'MLB2677257152',
     variations: [{
       url: 'http://http2.mlstatic.com/D_707977-MLB50757324427_072022-I.jpg'
     }]
   }, 
   {
+    id: 'MLB2814170446',
     variations: [{
       url: 'http://http2.mlstatic.com/D_906376-MLB42088169766_062020-I.jpg'
     }]
   }, 
   {
+    id: 'MLB2131125942',
     variations: [{
       url: 'http://http2.mlstatic.com/D_762049-MLB52350283800_112022-I.jpg'
     }]
